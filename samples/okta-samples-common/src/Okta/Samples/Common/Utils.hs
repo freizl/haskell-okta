1bd5359b51852b057aa2e149aa28582e4fbddbef
Haisheng Wu <haisheng.wu@okta.com>
2019-10-15 23:12:09 -0700

    remove reading from config file

--- samples/okta-samples-common/src/Okta/Samples/Common/Utils.hs
+++ samples/okta-samples-common/src/Okta/Samples/Common/Utils.hs
@@ -1,24 +1,9 @@
 module Okta.Samples.Common.Utils where
 
--- import           Control.Lens               ((^.))
 import qualified Data.Aeson         as Aeson
 import           Data.ByteString    (ByteString)
--- import qualified Data.ByteString.Lazy.Char8 as BS
 import qualified Data.Text.Encoding as T
 import qualified Data.Text.Lazy     as TL
-
--- import           Okta.Samples.Common.Types
-
--- configFile :: String
--- configFile = ".samples.config.json"
-
--- readConfigFile :: IO (Either String Config)
--- readConfigFile = do
---   file <- BS.readFile configFile
---   return $ case Aeson.eitherDecode file of
---     Left e   -> Left e
---     Right sc -> Right (sc ^. oktaSampleConfig)
-
 
 tlToBS :: TL.Text -> ByteString
 tlToBS = T.encodeUtf8 . TL.toStrict
